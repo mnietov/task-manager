b4fbe3da9b300c22e85b2dacac98d249b930d661
Marc Nieto <Marc Nieto>
2016-05-24 20:23:23 +0200

    views

--- src/My/TaskBundle/Controller/DefaultController.php
+++ src/My/TaskBundle/Controller/DefaultController.php
@@ -3,11 +3,61 @@
 namespace My\TaskBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use My\TaskBundle\Entity\Task;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TaskBundle:Default:index.html.twig');
+        $repository = $this->getDoctrine()->getRepository('TaskBundle:Task');
+        $tasks = $repository->findAll();
+        
+        return $this->render('TaskBundle:Default:index.html.twig', array('tasks' => $tasks));
+    }
+    
+    public function showAddFormAction()
+    {
+        return $this->render('TaskBundle:Default:add.html.twig');
+    }
+    
+    public function addAction(Request $request)
+    {
+        $task = new Task();
+        $task->setName($request->request->get('name'));
+        $task->setDescription($request->request->get('description'));
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($task);
+        $em->flush();
+        return $this->redirect($this->generateUrl('task_homepage'));
+    }
+    
+    public function showEditFormAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('TaskBundle:Task');
+        $task = $repository->find($id);
+        return $this->render('TaskBundle:Default:edit.html.twig', array('task' => $task));
+    }
+    
+    public function editAction($id, Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository('TaskBundle:Task');
+        $task = $repository->find($id);
+        $task->setName($request->request->get('name'));
+        $task->setDescription($request->request->get('description'));
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->persist($task);
+        $em->flush();
+        return $this->redirect($this->generateUrl('task_homepage'));
+    }
+    
+    public function deleteAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('TaskBundle:Task');
+        $task = $repository->find($id);
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->remove($task);
+        $em->flush();
+        return $this->redirect($this->generateUrl('task_homepage'));
     }
 }
